4c486bf52ddff0c8e4ac55e25a65910ba8494d96
Jordan Martin <jordanmartinksu@gmail.com>
2020-08-24 01:36:27 -0500

    added prod build

---
+++ precache-manifest.59775556d9bf3012cb70725df4d6008f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ebccc51354bc6ce9a39",
+    "url": "/react-resume/static/js/runtime~main.bbd03709.js"
+  },
+  {
+    "revision": "8189121d742edbe44f1b",
+    "url": "/react-resume/static/js/main.484bb1b0.chunk.js"
+  },
+  {
+    "revision": "4308e8054f9ccddf274e",
+    "url": "/react-resume/static/js/2.f732d941.chunk.js"
+  },
+  {
+    "revision": "8189121d742edbe44f1b",
+    "url": "/react-resume/static/css/main.63be118b.chunk.css"
+  },
+  {
+    "revision": "bf04fd239b3db48ad2ed4d20ca222d50",
+    "url": "/react-resume/index.html"
+  }
+];
